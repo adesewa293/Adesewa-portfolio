d46af46f66ce4d1d85e80c897b2dbdaf5a7f2031
Adesewa Aishat <adesewaaishat@gmail.com>
2023-11-06 02:20:04 +0100

    fix close button

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -71,38 +71,47 @@ export default function Header() {
           RESUME
         </Link>
       </div>
-      <div className={`flex flex-col md:hidden items-center absolute top-0 left-0 bg-gray-400 w-full ${isMobileMenuOpen ? "hidden" : "flex"}`}>
-        <Link
-          className="mr-4 hover:underline hover:decoration-dotted underline-offset-8"
-          href="/"
-        >
-          Home
-        </Link>
-        <Link
-          className="mr-4 hover:underline hover:decoration-dotted underline-offset-8"
-          href="#about"
-        >
-          About
-        </Link>
-        <Link
-          className="mr-4 hover:underline hover:decoration-dotted underline-offset-8"
-          href="#contact"
-        >
-          Contact
-        </Link>
-        <Link
-          className="mr-4 hover:underline hover:decoration-dotted underline-offset-8"
-          href="#projects"
-        >
-          Projects
-        </Link>
-        <Link
-          target="_blank"
-          href="/assets/resume.pdf"
-          className="border-amber-700 border-2 rounded-lg p-3"
-        >
-          RESUME
-        </Link>
+      <div
+        className={`flex flex-col md:hidden absolute top-0 left-0 bg-gray-400 w-full ${
+          isMobileMenuOpen ? "hidden" : "flex"
+        }`}
+      >
+        <div className="self-end" onClick={toggleMobileMenu}>
+          <img width="20px" src="/close-icon.png" />
+        </div>
+        <div className="flex flex-col items-center">
+          <Link
+            className="mr-4 hover:underline hover:decoration-dotted underline-offset-8"
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            className="mr-4 hover:underline hover:decoration-dotted underline-offset-8"
+            href="#about"
+          >
+            About
+          </Link>
+          <Link
+            className="mr-4 hover:underline hover:decoration-dotted underline-offset-8"
+            href="#contact"
+          >
+            Contact
+          </Link>
+          <Link
+            className="mr-4 hover:underline hover:decoration-dotted underline-offset-8"
+            href="#projects"
+          >
+            Projects
+          </Link>
+          <Link
+            target="_blank"
+            href="/assets/resume.pdf"
+            className="border-amber-700 border-2 rounded-lg p-3"
+          >
+            RESUME
+          </Link>
+        </div>
       </div>
     </div>
   );
