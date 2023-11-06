9644b6df4822362e82d4ddf74f04a82a628e1c76
Adesewa Aishat <adesewaaishat@gmail.com>
2023-11-06 14:01:51 +0100

    fix toggle icon

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -22,21 +22,25 @@ export default function Header() {
         aria-expanded={isMobileMenuOpen}
       >
         <span className="sr-only">Open main menu</span>
-        <svg
-          className="w-5 h-5"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 17 14"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M1 1h15M1 7h15M1 13h15"
-          />
-        </svg>
+        {isMobileMenuOpen ? (
+          <img width="20px" src="/close-icon.png" />
+        ) : (
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        )}
       </button>
       <div className="hidden md:block mt-4 text-white">
         <Link
